d00693b103b7db5107193471d53d1a439f8074ef
fwv <fengwei1@mail.ustc.edu.cn>
2021-01-05 20:17:24 +0800

    [add]网络基础组件

--- network/broadcaster.go
+++ network/broadcaster.go
@@ -1,18 +1,23 @@
 package network
 
-import "github.com/LILILIhuahuahua/ustc_tencent_game/network/kcp"
+import (
+	"fmt"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/network/kcpnet"
+	"github.com/xtaci/kcp-go"
+	"log"
+)
 
 //网络组件----广播者
 type(
 	Broadcaster struct {
 		addr string
-		server *kcp.KcpServer
+		server *kcpnet.KcpServer
 		conctmap map[interface{}]*Connector
 	}
 )
 //数据持有；连接者指针列表
 func NewBroadcaster(address string) (*Broadcaster,error){
-	s,err := kcp.NewKcpServer(address)
+	s,err := kcpnet.NewKcpServer(address)
 	if err != nil {
 		return nil,err
 	}
@@ -23,13 +28,13 @@ func NewBroadcaster(address string) (*Broadcaster,error){
 	},nil
 }
 
-func(b *Broadcaster) Serv() error{
-	err := b.server.Run()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+//func(b *Broadcaster) Serv() error{
+//	err := b.server.Run()
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
 
 //注册连接者
 func (b *Broadcaster)RegisterConnector(c *Connector)  error{
@@ -48,9 +53,45 @@ func (b *Broadcaster)NotifyAll(buff []byte) error {
 	for  _,connector := range b.conctmap{
 		err:= connector.Update(buff)
 		if nil != err {
+			println(err)
 			return err
 		}
 	}
  	return nil
+}
+
+
+func (b *Broadcaster) Serv() error {
+	for {
+		conn, err := b.server.Listen.AcceptKCP()
+		if err != nil {
+			return err
+		}
+		connector := NewConnector(b,*conn)
+		err = b.RegisterConnector(connector)
+		if err != nil {
+			return err
+		}
+		go b.Handle(conn)
+	}
+}
+
+func (b *Broadcaster) Handle(conn *kcp.UDPSession) {
+	buf := make([]byte, 4096)
+	for {
+		n, err := conn.Read(buf)
+		fmt.Println(string(buf))
+		// 处理buf，改为事件驱动型...
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		err = b.NotifyAll(buf[:n])
+		//n, err = conn.Write(buf[:n])
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 }
 
