ba51beb68b2aea6f39bd58e74594f9ad1c54c7c4
song <s1322023401@gmail.com>
2021-02-17 15:42:21 +0800

    [test] 修改测试用例

--- framework/test/client_test.go
+++ framework/test/client_test.go
@@ -1,7 +1,14 @@
 package test
 
 import (
+	pb "github.com/LILILIhuahuahua/ustc_tencent_game/api/proto"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/internal/event"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/internal/event/info"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/internal/event/request"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/tools"
+	"github.com/golang/protobuf/proto"
 	"github.com/xtaci/kcp-go"
+	"io"
 	"log"
 	"testing"
 	"time"
@@ -12,13 +19,37 @@ func TestConnect(t *testing.T) {
 	// wait for server to become ready
 	time.Sleep(time.Second)
 
+	msg := event.GMessage{
+		MsgType:     tools.MsgTypeRequest,
+		GameMsgCode: tools.EntityInfoChangeRequest,
+		SessionId:   0,
+		SeqId:       0,
+		Data:        &request.EntityInfoChangeRequest{
+			EventType:  0,
+			HeroId:     0,
+			LinkedId:   0,
+			LinkedType: "",
+			HeroMsg:    info.HeroInfo{},
+		},
+	}
+
+	pbMsg := msg.ToMessage().(*pb.GMessage)
+	out, err := proto.Marshal(pbMsg)
+	if err != nil {
+		t.Fatalf("get %s", err)
+	}
+	var buf []byte
 	// dial to the echo server
-	if sess, err := kcp.DialWithOptions(addr, nil, 10, 3); err == nil {
+	if sess, err := kcp.DialWithOptions(addr, nil, 0, 0); err == nil {
 		for {
 			data := time.Now().String()
-			//buf := make([]byte, len(data))
 			log.Println("[Client SENT]: ", data)
-			if _, err := sess.Write([]byte(data)); err != nil {
+			if _, err := sess.Write(out); err != nil {
+				log.Println(err.Error())
+			}
+			if _, err := io.ReadFull(sess, buf); err == nil {
+				log.Println("get from server", string(buf))
+			} else {
 				log.Println(err.Error())
 			}
 			time.Sleep(time.Second)
