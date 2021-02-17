669e73bd2a12e5c956cbc47df7726305bb8b9ddf
song <s1322023401@gmail.com>
2021-02-17 15:47:29 +0800

    [GMessage] 添加GMessage中ToMessage方法

--- internal/event/GMessage.go
+++ internal/event/GMessage.go
@@ -4,20 +4,61 @@ import (
 	pb "github.com/LILILIhuahuahua/ustc_tencent_game/api/proto"
 	"github.com/LILILIhuahuahua/ustc_tencent_game/framework"
 	e "github.com/LILILIhuahuahua/ustc_tencent_game/framework/event"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/internal/event/notify"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/internal/event/request"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/tools"
 )
 
 type GMessage struct {
 	framework.BaseEvent
-	MsgType int32
-	//GameMsgCode int32
-	//SessionId int64
-	SeqId int32
-	Data  e.Event
+	MsgType     int32
+	GameMsgCode int32
+	SessionId   int32
+	SeqId       int32
+	Data        e.Event
 }
 
 func (this *GMessage) ToMessage() interface{} {
-	//todo:
-	panic("implement me")
+	var pbMsgNotify *pb.Notify
+	var pbMsgRequest *pb.Request
+	var pbMsgResponse *pb.Response
+
+	switch this.MsgType {
+	case tools.MsgTypeNotify:
+		switch this.Data.(type) { //这里的处理函数可以进行封装
+		case *notify.EntityInfoChangeNotify:
+			pbMsgNotify = &pb.Notify{
+				EntityInfoChangeNotify: this.Data.ToMessage().(*pb.EntityInfoChangeNotify),
+			}
+
+		default:
+			panic("no match type")
+		}
+		break
+	case tools.MsgTypeRequest:
+		switch this.Data.(type) {
+		case *request.EntityInfoChangeRequest:
+			pbMsgRequest = &pb.Request{
+				EntityChangeRequest: this.Data.ToMessage().(*pb.EntityInfoChangeRequest),
+			}
+		}
+		break
+	case tools.MsgTypeResponse:
+		break
+	default:
+		panic("msg type is incorrect")
+	}
+	pbMsg := &pb.GMessage{
+		MsgType:   pb.MSG_TYPE(this.MsgType),
+		MsgCode:   pb.GAME_MSG_CODE(this.GameMsgCode),
+		SessionId: this.SessionId,
+		SeqId:     this.SeqId,
+		Notify:    pbMsgNotify,
+		Request:   pbMsgRequest,
+		Response:  pbMsgResponse,
+	}
+
+	return pbMsg
 }
 
 func (this *GMessage) CopyFromMessage(obj interface{}) e.Event {
