ef079e05138d8f9cf737cb43e89ae7f1d1e7c124
fwv <fengwei1@mail.ustc.edu.cn>
2021-05-15 19:17:51 +0800

    [add]添加房间信息

--- internal/event/info/RoomInfo.go
+++ internal/event/info/RoomInfo.go
@@ -1,14 +1,46 @@
 package info
 
 import (
+	pb "github.com/LILILIhuahuahua/ustc_tencent_game/api/proto"
 	"github.com/LILILIhuahuahua/ustc_tencent_game/framework"
+	"github.com/LILILIhuahuahua/ustc_tencent_game/framework/event"
 )
 
 type RoomInfo struct {
 	framework.BaseEvent
 	ID           int32
-	Status	     int32
+	Status       int32
 	CreateTime   int64
 	PlayerCount  int32
 	HighestScore int32
+}
+
+func (r RoomInfo) ToMessage() interface{} {
+	return &pb.RoomMsg{
+		Id: r.ID,
+		Status: r.Status,
+		CreateTime: r.CreateTime,
+		PlayerCount: r.PlayerCount,
+		HighestScore: r.HighestScore,
+	}
+}
+
+func (r RoomInfo) FromMessage(obj interface{}) {
+	pbMsg := obj.(*pb.RoomMsg)
+	r.ID = pbMsg.Id
+	r.Status = pbMsg.Status
+	r.CreateTime = pbMsg.CreateTime
+	r.PlayerCount = pbMsg.PlayerCount
+	r.HighestScore = pbMsg.HighestScore
+}
+
+func (r RoomInfo) CopyFromMessage(obj interface{}) event.Event {
+	pbMsg := obj.(*pb.RoomMsg)
+	return &RoomInfo{
+		ID: pbMsg.Id,
+		Status: pbMsg.Status,
+		CreateTime: pbMsg.CreateTime,
+		PlayerCount: pbMsg.PlayerCount,
+		HighestScore: pbMsg.HighestScore,
+	}
 }
